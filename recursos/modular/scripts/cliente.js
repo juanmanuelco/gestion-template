d03f9fe48857129104536f1b467a1a6f703b7c92
PATITO\JUNIORCEDE <juniorcede2@hotmail.com>
2017-09-04 00:41:56 -0500

    Arreglo en el codigo de script cliente.js

--- recursos/modular/scripts/cliente.js
+++ recursos/modular/scripts/cliente.js
@@ -1,13 +1,19 @@
+//se obtiene el input del campo de cedula
 var inputCed = document.getElementById("Ced_Cli");
 if (inputCed) { 
+	//se le agrega el evento blur con la funcion de verfificarCliente
 	inputCed.addEventListener("blur",VerificarCliente)
 };
 var textoAnterior={}
+//Se verifica si el cliente existe o no en la base de datos
 function VerificarCliente(e) {
+	//se obtiene la cedula
 	var cedula = this.value;
-
+	//si los caracteres ingresados en el input son 10,13 o 14 se ejecuta la condicion
 	if (cedula.length == 10 || cedula.length == 13 || cedula.length == 14) {
+		//obtenemos el div padre del input
 		var divPadre = this.parentNode
+		//si el div padre contiene la subclase "is-invalid cancelamos la funcion"
 		if (divPadre.classList.contains("is-invalid")) {return false;};
 		var span = divPadre.getElementsByTagName("span");
 		if (span && textoAnterior[this.id] == undefined) {
@@ -20,7 +26,6 @@ function VerificarCliente(e) {
 	    },
 	    function(data,status){
 	    	if (status == "success") {
-	    		console.log(data)
 	    		if (data.length >= 1) {
 	    			if (cedula.length == 10) {
 						span[0].innerHTML = "El cliente con esta cédula ya está registrado en la base de datos";
@@ -45,13 +50,10 @@ function VerificarCliente(e) {
 	};
 }
 
-
-FuncionesCliente={}
-
 //funcion para agregar clientes
-FuncionesCliente["saveCliente"] = function (e){
+function saveCliente (e,button){
 	e.preventDefault();
-	var form = this.form
+	var form = button.form
 	if (!form) {return false}
 	var bool = ValidarDatosFormulario(form);
 	console.log(bool)
@@ -75,14 +77,9 @@ FuncionesCliente["saveCliente"] = function (e){
 	}
 }
 
-
-//funcion pensada como funcion para el modulo de clientes el cual muestra un formulario en un modal
-//para editar los datos de un cliente
-
-
-
-FuncionesCliente["editClient"] = function () {
-	var divpadre = this.parentNode.parentNode
+function editClient (e,button) {
+	e.preventDefault();
+	var divpadre = button.parentNode.parentNode
 	var divButton = divpadre.parentNode
 	var datos = divButton.parentNode.getElementsByTagName("td")
 	// agregar en el form un action con la ruta del admin
@@ -147,14 +144,15 @@ FuncionesCliente["editClient"] = function () {
 		  	}
 		});
 		setTimeout(function (a){
-			FuncionesCliente.init();
+			Funciones.init();
 		},200);
 }
 
 //funcion pensada como funcion para el modulo de clientes el cual muestra un formulario en un modal
 //para borrar los datos de un cliente
-FuncionesCliente["deleteClient"] = function() {
-	var divpadre = this.parentNode.parentNode
+function deleteClient(e,button) {
+	e.preventDefault();
+	var divpadre = button.parentNode.parentNode
 	var divButton = divpadre.parentNode
 	var datos = divButton.parentNode.getElementsByTagName("td")
 	var infoHTML = '<form id="deleteForm" action="/admin/deleteClient" method="post"><label>Cedula: '+datos[0].innerHTML+
@@ -198,73 +196,9 @@ FuncionesCliente["deleteClient"] = function() {
     });
 }
 
-
-//funcion inicializacion pensada para poder ser llamada en el caso de que se genere nuevos elementos html 
-//desde javascript
-FuncionesCliente["init"] = function (argument) {
-	// se a cambiado la forma de obtener los elementos del html
-	// en este caso se recorre por tipo de elemento ya que lo anterior no identificaba los buttons que estaban
-	// dentro de tablas como en el inentario
-	// se define la variable elements donde se guardaran todos los elementos
-	var elements=[]
-	// se define los elementos a buscar por medio del tagname
-	var inputs = document.getElementsByTagName("input");
-	var button = document.getElementsByTagName("button");
-	var divs = document.getElementsByTagName("div");
-	// con los for se recorren y se insertan en el arrat "elements"
-	for (var i = 0; i < inputs.length; i++) {elements.push(inputs[i])};
-	for (var i = 0; i < button.length; i++) {elements.push(button[i])};
-	for (var i = 0; i < divs.length; i++) {elements.push(divs[i])};
-	// con este for recorremos todos los elementos guardados en busca de la validacion y evento
-	for (var i = 0; i < elements.length; i++) {
-	var atributo = elements[i].getAttribute("validation") || false;
-
-		//Validacion de atributos solonum y solodecimal, por si se quiere usar estas 2 
-		//funciones mientras se usa otra como por ejemplo cedula
-		var solonum = elements[i].getAttribute("solonum") || false;
-		if (solonum) {
-			elements[i].addEventListener("keypress",FuncionesCliente["NumeroEntero"])
-			elements[i].addEventListener("keyup",FuncionesCliente["NumeroEntero"])
-		};
-		var solodecimal = elements[i].getAttribute("solodecimal") || false;
-		if (solodecimal) {
-			elements[i].addEventListener("keypress",FuncionesCliente["NumDecimal"])
-			elements[i].addEventListener("keyup",FuncionesCliente["NumDecimal"])
-		};
-		var soloLetras = elements[i].getAttribute("soloLetras") || false;
-		if (soloLetras) {
-			elements[i].addEventListener("keypress",FuncionesCliente["soloLetras"])
-			elements[i].addEventListener("keyup",FuncionesCliente["soloLetras"])
-		};
-		var EnterNext = elements[i].getAttribute("EnterNext") || false;
-		if (EnterNext) {elements[i].addEventListener("keyup",FuncionesCliente["EnterNext"])};
-
-		if ( atributo && FuncionesCliente[atributo] ){
-			var evento = elements[i].getAttribute("event") || false;
-			//modificacion del codigo para poder agregar más de 1 evento a las funciones
-			if (evento) {
-				var arrayEvent = evento.split(",")
-				if (arrayEvent.length > 1) {
-					for (var i = 0; i < arrayEvent.length; i++) {
-						elements[i].addEventListener(arrayEvent[i],FuncionesCliente[atributo]);
-					};
-				}
-				else{
-					elements[i].addEventListener(evento,FuncionesCliente[atributo]);
-				}
-			}
-			elements[i].addEventListener("change",FuncionesCliente[atributo]);
-			/*elements[i].addEventListener("paste", function (e) {
-				e.preventDefault();
-				return false;
-			});*/
-		}
-	}
-}
-
-
-FuncionesCliente["infoCliente"] = function (argument) {
-	var divpadre = this.parentNode.parentNode
+function infoCliente(e,button) {
+	e.preventDefault();
+	var divpadre = button.parentNode.parentNode
 	var divButton = divpadre.parentNode
 	var datos = divButton.parentNode.getElementsByTagName("td")
 	console.log(datos)
@@ -306,7 +240,3 @@ FuncionesCliente["infoCliente"] = function (argument) {
 	  	closeOnConfirm: true
 	});
 } 
-
-
-//inicializa la funcion que recorre el html en busca de los elementos con los atributos explicados
-FuncionesCliente.init();
